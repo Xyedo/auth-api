2c2f85274263144462181243dee01d16bb8b7ff7
Xyedo <hafidmahdi23@gmail.com>
2022-12-06 10:03:04 +0700

    chore:fix typo

--- src/infrastructures/container.js
+++ src/infrastructures/container.js
@@ -12,7 +12,7 @@ const UserRepositoryPostgres = require("./repository/userRepositoryPostgres");
 const BcryptPasswordHash = require("./security/bcryptPasswordHash");
 
 // use case
-const AddUserUseCase = require("../applications/use_case/addUserUseCase");
+const AddUserUseCase = require("../applications/use_case/addUserUsecase");
 const UserRepository = require("../domains/users/userRepository");
 const PasswordHash = require("../applications/security/passwordHash");
 
